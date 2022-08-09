5af72d53140aa05d3e2083c19482c7a36e4bd861
Tran Huy <huytran161201@gmail.com>
2022-08-09 09:17:38 +0700

    config2

--- Request.Infrastructure/Data/ApplicationDbContext.cs
+++ Request.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Request.Infrastructure.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        private readonly IMediator _mediator;
+
+        public ApplicationDbContext(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<RequestType> RequestTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=192.168.2.231;database=Intern_RequestModule;user id=sa;password=vStation123;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<LeaveRequest>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<RequestType>().HasQueryFilter(p => !p.IsDelete);
+            modelBuilder.Entity<State>().HasQueryFilter(p => !p.IsDelete);
+        }
+
+        public async Task SaveEntitiesAsync()
+        {
+            await SaveChangesAsync();
+            await _mediator.DispatchDomainEventsAsync(this);
+        }
     }
 }
